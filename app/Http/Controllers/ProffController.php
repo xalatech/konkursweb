3a846ff7631c5ebc085d4507bc75d50599f8dccf
Ibrahim Rahmani <ibrahim@xala.no>
2020-05-21 03:31:50 +0200

    Use only http request

--- app/Http/Controllers/ProffController.php
+++ app/Http/Controllers/ProffController.php
@@ -3,22 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
-use App\Util\Proff;
-use Facade\FlareClient\Http\Response;
+use Illuminate\Support\Facades\Http;
 
 class ProffController extends Controller
 {
     protected $proff;
+    public $baseURI;
+    public $token;
 
-    public function __construct(Proff $proff)
+    public function __construct()
     {
-        $this->proff = $proff;
+        $this->token = env('PROFF_TOKEN');
+        $this->baseURI = env('PROFF_BASE_URL');
     }
 
     public function hent($org_nr)
     {
-        $enhet = $this->proff->hent_enhet($org_nr);
-        return response()->json($enhet, 200);
+        $response = Http::withToken($this->token)->get($this->baseURI . '/' . $org_nr);
+        return response()->json($response, 200);
     }
 }
